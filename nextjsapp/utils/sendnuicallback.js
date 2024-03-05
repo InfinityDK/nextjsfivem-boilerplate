c6ea4fa01203c4421fc4d9569dc82c3b4fbb0cea
Valdemar <41428728+ValdemarLarsen@users.noreply.github.com>
2024-03-05 21:46:19 +0100

    Update sendnuicallback.js
    Added catch for the SendNuiCallback

--- nextjsapp/utils/sendnuicallback.js
+++ nextjsapp/utils/sendnuicallback.js
@@ -1,13 +1,19 @@
 const SendNuiCallback = async (endpoint, data, cb) => {
-    await fetch(`https://${process.env.NEXT_PUBLIC_RESOURCENAME}/${endpoint}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify((data ? data : {}))
-    }).then(response => response.json()).then((data) => {
-        cb(data)
-    });
-}
+    try {
+        const response = await fetch(`https://${process.env.NEXT_PUBLIC_RESOURCENAME}/${endpoint}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data || {})
+        });
+        const responseData = await response.json();
+        cb(responseData);
+    } catch (error) {
+        console.error('Fejl i callback der sender forkert data', error);
+    }
+};
 
 export default SendNuiCallback;
+
+// Currently working on a fix on the error so for now we just log it.
